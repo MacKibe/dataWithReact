49b3263cdbda01c2e817fd631d4c62687f58645f
Ndichu James <ndichujames2030@gmail.com>
2023-11-07 05:59:46 +0300

    cleared some files that I'll not use

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import React from "react";
-import Home from './pages/Home';
 
 function App() {
   return (
--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import React from 'react';
 // 
 // This renders the application to the broswer.
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
